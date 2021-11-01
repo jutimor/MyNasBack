ac8cee876dc686372d1535687af201e9402bc6a1
julien THOREZ <julien.thorez@outlook.com>
2021-11-01 10:15:51 +0100

    test jasmine

---
+++ spec/support/downloads.spec.js
@@ -0,0 +1,11 @@
+describe('Downloads', () => {
+    beforeEach(() => {
+        // cache.clear();
+    });
+    afterEach(() => {
+        // cache.clear();
+    });
+    it('should be true', function () {
+        expect(true).toBeTruthy();
+    });
+});
